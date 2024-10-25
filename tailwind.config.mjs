f93e3c6a344aa392860d08a4782d1bfe6e6e7baa
Andres Prieto <andresprieto2703@gmail.com>
2024-10-25 08:31:54 -0500

    ADD: Tailwind config
    Adding tailwind config for Astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      background: "#09090d",
+      accent: "#0bc7c3",
+      text: "#ffffff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
