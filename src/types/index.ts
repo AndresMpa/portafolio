4c2f2e449bb8881900ac352748c8a849c6f8ccf2
Andres Prieto <andresprieto2703@gmail.com>
2024-10-29 11:29:10 -0500

    ADD: Types
    Adding types to their respective file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,46 @@
+interface HeaderBtnProps {
+  icon: any;
+  alt: string;
+}
+
+interface DockRouteProps {
+  name: string;
+  ref: string;
+}
+
+interface ProjectDescriptionProps {
+  title: string;
+  context: string;
+  authors?: {
+    link: string;
+    user: string;
+  }[];
+}
+
+interface ProjectLinkProps {
+  url: string;
+  image: any;
+  topics: string;
+  alt?: string;
+  flip?: boolean;
+}
+
+interface ProjectProps {
+  description: ProjectDescriptionProps;
+  link: ProjectLinkProps;
+}
+
+interface ReferenceProps {
+  icon: any;
+  link: string;
+  alt: string;
+  width?: number;
+  useSvg?: boolean;
+  useAlt?: boolean;
+  styledClass?: string;
+  underline?: boolean;
+}
+
 interface TableItemProps {
   type: "chip" | "reference" | "title";
   ref: boolean;
@@ -5,10 +48,18 @@ interface TableItemProps {
   context: string;
 }
 
-
 interface TableHeadersProps {
   headers: string[];
   entries: Array<TableItemProps[]>;
 }
 
-export type { TableItemProps, TableHeadersProps };
+export type {
+  HeaderBtnProps,
+  DockRouteProps,
+  ProjectProps,
+  ProjectDescriptionProps,
+  ProjectLinkProps,
+  ReferenceProps,
+  TableItemProps,
+  TableHeadersProps,
+};
