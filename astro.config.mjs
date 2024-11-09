5d41e5dc7c331661c01a155884d4b175c744618d
Andres Prieto <andresprieto2703@gmail.com>
2024-11-08 19:21:00 -0500

    ADD: i18n
    Adding stuff for i18n internacionalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import liciousI18n from "@astrolicious/i18n";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://andresmpa.github.io/',
-  base: "/portafolio/"
+  integrations: [
+    tailwind(),
+    liciousI18n({
+      defaultLocale: "en",
+      locales: ["en", "es"],
+      client: {
+        data: true,
+      },
+    }),
+  ],
+  site: "https://andresmpa.github.io/",
+  base: "/",
 });
