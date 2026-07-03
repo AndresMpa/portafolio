03ba257f45093ca7e647da17e21ce6855470e089
Andres M. Prieto <andresprieto2703@gmail.com>
2026-07-03 17:29:43 -0500

    FIX: Extending

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      background: "#09090d",
-      accent: "#0bc7c3",
-      text: "#ffffff",
+    extend: {
+      colors: {
+        background: "#09090d",
+        accent: "#0bc7c3",
+        text: "#ffffff",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
