9527b16fb371d54bdd6c1a754e0f052ff0df5895
Andres M. Prieto <andresprieto2703@gmail.com>
2026-07-03 12:31:51 -0500

    Update: Improvement
    Improving the site for AI Agent and general improvements for SEO and GEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 import liciousI18n from "@astrolicious/i18n";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://andresmpa.netlify.app",
   integrations: [
     tailwind(),
+    sitemap(),
     liciousI18n({
       defaultLocale: "en",
       locales: ["en", "es"],
@@ -17,6 +19,5 @@ export default defineConfig({
       },
     }),
   ],
-  output: "server",
-  adapter: netlify(),
+  output: "static",
 });
