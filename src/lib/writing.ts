6ca1b46e25364093a15955c8deb973c051fc1b1c
Andres M. Prieto <andresprieto2703@gmail.com>
2026-07-16 04:15:40 -0500

    MILESTONE: Stable
    New version ready to ship

--- src/lib/writing.ts
+++ src/lib/writing.ts
@@ -8,3 +8,8 @@ export async function getLatestWriting(locale: string, limit = 4) {
     .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
     .slice(0, limit);
 }
+
+export async function getAllWriting(locale: string) {
+  const posts = await getCollection("writing", (e) => e.data.locale === locale);
+  return posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+}
